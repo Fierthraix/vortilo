533b9433e22e324c925720a8705bee4c46eb5bb5
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2021-05-26 16:16:19 -0400

    Nova biblioteko: serde por la JSON

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,18 @@ use vortilo::{Rule, Vortilo};
 
 fn main() {
     // Legu dosieron.
-    let dosiero = "Mi estas homo kiuj simple ŝatas la plej bonajn aĵojn en la viv'".to_ascii_lowercase();
+    //let dosiero = "Mi estas homo kiuj simple ŝatas la plej bonajn aĵojn en la viv'".to_ascii_lowercase();
 
-    let frazo: HashMap<String, Vec<(String, String)>> = HashMap::new();
+    //let frazo: HashMap<String, Vec<(String, String)>> = HashMap::new();
 
     // Elektu lingvon.
 
+    let dosiero = "neniujn tiaj";
     for vorto in dosiero.split_whitespace() {
-        let parsita_vorto = Vortilo::parse(Rule::vorto, vorto);
+        let parsita_vorto = Vortilo::parse(Rule::vorto, vorto).unwrap().next().unwrap();
 
-        println!("{:?}", parsita_vorto);
+        //println!("{:?}", parsita_vorto);
+        println!("{:?}", kreu_propraĵoj(parsita_vorto));
     }
-
-    kreu_propraĵoj();
 
 }
